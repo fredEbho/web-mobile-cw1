682b7b4c8346a20a056b5cc26c611ef68f619ff7
fredebho <fredebho2017@gmail.com>
2024-01-31 16:34:58 +0400

    change subject to topic

--- js/lessons.js
+++ js/lessons.js
@@ -1,92 +1,92 @@
  let lessons = [
   {
     "id": 1001,
-    "subject": "Math",
+    "topic": "Math",
     "location": "London",
     "price": 2800,
-    "icon" :"fas fa-swatchbook",
-    "spaces": 5,
+    "image" :"london_lesson.jpg",
+    "spaces": 10,
     "cart_quantity": 0
   },
    {
      "id": 1002,
-     "subject": "English",
+     "topic": "English",
      "location": "Dubai",
      "price": 2000,
-     "icon" :"fas fa-torah",
-     "spaces": 5,
+     "image" :"dubai_lesson.jpg",
+     "spaces": 10,
      "cart_quantity": 0
    },
    {
      "id": 1003,
-     "subject": "Physics",
+     "topic": "Physics",
      "location": "Madrid",
      "price": 2300,
-     "icon" :"fas fa-atlas",
-     "spaces": 5,
+     "image" :"malta_lesson.jpg",
+     "spaces": 10,
      "cart_quantity": 0
    },
    {
      "id": 1004,
-     "subject": "Chemistry",
+     "topic": "Chemistry",
      "location": "Barcelona",
      "price": 2400,
-     "icon" :"fas fa-book",
-     "spaces": 5,
+     "image" :"malta_lesson.jpg",
+     "spaces": 10,
      "cart_quantity": 0
    },
    {
    "id": 1005,
-   "subject": "Biology",
+   "topic": "Biology",
    "location": "Helsinki",
    "price": 2550,
-   "icon" :"fas fa-book-medical",
-   "spaces": 5,
+   "image" :"dubai_lesson.jpg",
+   "spaces": 10,
    "cart_quantity": 0
  },
    {
      "id": 1006,
-     "subject": "Commerce",
+     "topic": "Commerce",
      "location": "Valletta",
      "price": 2100,
-     "icon" :"fas fa-book",
-     "spaces": 5,
+     "image" :"london_lesson.jpg",
+     "spaces": 10,
      "cart_quantity": 0
    },
    {
      "id": 1007,
-     "subject": "History",
+     "topic": "History",
      "location": "Nairobi",
      "price": 1800,
-     "icon" :"fas fa-quran",
-     "spaces": 5,
+     "image" :"dubai_lesson.jpg",
+     "spaces": 10,
      "cart_quantity": 0
    },
    {
      "id": 1008,
-     "subject": "Economics",
+     "topic": "Economics",
      "location": "Lagos",
      "price": 1900,
-     "icon" :"fas fa-book",
-     "spaces": 5,
+     "image" :"dubai_lesson.jpg",
+     "spaces": 10,
      "cart_quantity": 0
    },
    {
      "id": 1009,
-     "subject": "Further Mathematics",
+     "topic": "Further Mathematics",
      "location": "Sao Paulo",
      "price": 2900,
-     "icon" :"fas fa-book",
-     "spaces": 5,
+     "image" :"malta_lesson.jpg",
+     "spaces": 10,
      "cart_quantity": 0
    },
    {
      "id": 1010,
-     "subject": "Agricultural Science",
+     "topic": "Agricultural Science",
      "location": "Delhi",
      "price": 2600,
-     "icon" :"fas fa-book",
-     "spaces": 5,
+     "image" :"malta_lesson.jpg",
+     "spaces": 10,
      "cart_quantity": 0
    }
 ]
